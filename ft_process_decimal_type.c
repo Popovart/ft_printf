74397c2153c78617205ce4a17c498ffa4fc3a2c1
Dmitrii <gmdmintry@gmail.com>
2024-10-12 19:22:54 +0200

    INT_MIN now works correctly

--- ft_process_decimal_type.c
+++ ft_process_decimal_type.c
@@ -6,7 +6,7 @@
 /*   By: dmitrii <dmitrii@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:12:22 by dmitrii           #+#    #+#             */
-/*   Updated: 2024/10/11 21:00:30 by dmitrii          ###   ########.fr       */
+/*   Updated: 2024/10/12 19:22:17 by dmitrii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@ int	ft_process_decimal_type(va_list args, t_flags *flags)
 {
 	int		count;
 	long	decimal_num;
+	char	*num_str;
 
 	decimal_num = va_arg(args, int);
-	if (decimal_num < 0)
+	if (decimal_num < 0 && decimal_num != -2147483648)
 	{
 		flags->sign = "-";
 		decimal_num *= -1;
 	}
 	if (decimal_num != 0 || flags->precision != 0)
 	{
-		if (ft_add_strtolst(&flags->result, ft_itoa(decimal_num),
-				&ft_lstadd_back) == -1)
+		if (decimal_num == -2147483648)
+			num_str = ft_strdup("-2147483648");
+		else
+			num_str = ft_itoa(decimal_num);
+		if (ft_add_strtolst(&flags->result, num_str, &ft_lstadd_back) == -1)
 			return (-1);
 	}
 	count = ft_format_output_and_print(flags);
